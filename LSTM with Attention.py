efdd27a88d0e3ea03eacde9fa867df6b115d7915
keerti <keerti.mili@gmail.com>
2020-05-12 02:20:35 -0400

    adding model

--- LSTM with Attention.py
+++ LSTM with Attention.py
@@ -10,6 +10,13 @@ import pandas as pd
 from nltk.corpus import stopwords
 import re 
 from bs4 import BeautifulSoup 
+from tensorflow.keras.layers import Input, LSTM, Embedding, Dense, Concatenate, TimeDistributed
+from tensorflow.keras.models import Model
+from tensorflow.keras.callbacks import EarlyStopping
+import warnings
+pd.set_option("display.max_colwidth", 200)
+warnings.filterwarnings("ignore")
+
 
 
 data = pd.read_csv("Reviews.csv",nrows=10000)
@@ -214,5 +221,59 @@ for i in range(len(y_test)):
         st_en_del.append(i)
 x_test=np.delete(x_test,st_en_del, axis=0)
 y_test=np.delete(y_test,st_en_del, axis=0)
+
+#model
+
+latent_dim = 300
+embedding_dim=100
+
+# Encoder
+encoder_inputs = Input(shape=(max_text_len,))
+
+#embedding layer
+enc_emb =  Embedding(x_voc, embedding_dim,trainable=True)(encoder_inputs)
+
+#encoder lstm 1
+encoder_lstm1 = LSTM(latent_dim,return_sequences=True,return_state=True,dropout=0.4,recurrent_dropout=0.4)
+encoder_output1, state_h1, state_c1 = encoder_lstm1(enc_emb)
+
+#encoder lstm 2
+encoder_lstm2 = LSTM(latent_dim,return_sequences=True,return_state=True,dropout=0.4,recurrent_dropout=0.4)
+encoder_output2, state_h2, state_c2 = encoder_lstm2(encoder_output1)
+
+#encoder lstm 3
+encoder_lstm3=LSTM(latent_dim, return_state=True, return_sequences=True,dropout=0.4,recurrent_dropout=0.4)
+encoder_outputs, state_h, state_c= encoder_lstm3(encoder_output2)
+
+# Set up the decoder, using `encoder_states` as initial state.
+decoder_inputs = Input(shape=(None,))
+
+dec_emb_layer = Embedding(y_voc, embedding_dim,trainable=True)
+dec_emb = dec_emb_layer(decoder_inputs)
+
+decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True,dropout=0.4,recurrent_dropout=0.2)
+decoder_outputs,decoder_fwd_state, decoder_back_state = decoder_lstm(dec_emb,initial_state=[state_h, state_c])
+
+# Attention layer
+attn_layer = AttentionLayer(name='attention_layer')
+attn_out, attn_states = attn_layer([encoder_outputs, decoder_outputs])
+
+# Concat attention input and decoder LSTM output
+decoder_concat_input = Concatenate(axis=-1, name='concat_layer')([decoder_outputs, attn_out])
+
+#dense layer
+decoder_dense =  TimeDistributed(Dense(y_voc, activation='softmax'))
+decoder_outputs = decoder_dense(decoder_concat_input)
+
+# Define the model 
+model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+
+model.summary()
+
+model.compile(optimizer='rmsprop', loss='sparse_categorical_crossentropy')
+es = EarlyStopping(monitor='val_loss', mode='min', verbose=1,patience=2)
+
+history=model.fit([x_train,y_train[:,:-1]], y_train.reshape(y_train.shape[0],y_train.shape[1], 1)[:,1:] ,epochs=50,callbacks=[es],batch_size=128, validation_data=([x_test,y_test[:,:-1]], y_test.reshape(y_test.shape[0],y_test.shape[1], 1)[:,1:]))
+
 
 
